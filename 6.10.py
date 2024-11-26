caeaa44d200def95493c6eea5442d214f9216a40
Tolkovika <vkuropiatkina@gmail.com>
2024-11-26 15:44:10 +0100

    Initial commit: Dodanie plików

---
+++ 6.10.py
@@ -0,0 +1,10 @@
+###
+# String manipulation
+#
+movie = "The Lord of the Rings: The Return of the King"
+print('Number of characters: ', len(movie))
+print('Title in capital letters: ', movie.upper())
+print('Title in small letters: ', movie.lower())
+print('Number of times "e" appears: ', movie.count('e'))
+print('The word "Lord" is found at position: ', movie.find('Lord'))
+print('The word "dragon" is found at position: ', movie.find('dragon'))
