7ac4e5fe9b1beb4371445f825a1bf2d129d4ed84
Tolkovika <vkuropiatkina@gmail.com>
2024-11-26 13:00:56 +0100

    Initial commit: Dodanie plików

---
+++ Output/4.2.py
@@ -0,0 +1,9 @@
+# Calculate and print the income of a family per person
+
+# Family incomes and number of members
+father_income = 5450
+mother_income = 4920
+family_members = 5
+total_income = father_income + mother_income
+income_per_person = total_income / family_members
+print(f"Total family income is {total_income} , and income per person is {income_per_person}.")
